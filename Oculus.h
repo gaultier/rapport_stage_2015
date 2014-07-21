0021da7399a895f104e6846325ded6f88eae72ee
Philippe <philigaultier@gmail.com>
2014-07-21 14:44:02 +0200

    Add source

---
+++ Oculus.h
@@ -0,0 +1,399 @@
+#ifndef OCULUS_H
+#define OCULUS_H
+
+#include <GL/glew.h>
+#include <iostream>
+//To ignore the asserts uncomment this line:
+//#define NDEBUG
+#include <cassert>
+#include <cmath>
+#include <limits>
+#include <string>
+
+#include "Include/OVR/LibOVR/Include/OVR.h"
+#include "Include/OVR/LibOVR/Src/OVR_CAPI.h"
+#include "Include/OVR/LibOVR/Src/OVR_CAPI_GL.h"
+#include "Include/OVR/LibOVR/Src/Kernel/OVR_Math.h"
+
+#include "SDL2/SDL.h"
+
+#define GL3_PROTOTYPES 1
+#include "Include/GL3/gl3.h"
+
+#include "Include/glm/glm.hpp"
+
+#include "SDL2/SDL_syswm.h"
+
+#include "Utils.h"
+
+template<class T>
+class Oculus
+{
+
+public:
+    Oculus(T * scene):
+        scene_ {scene},
+        textureId_ {0},
+        FBOId_ {0},
+        depthBufferId_ {0},
+        hmd_ {0},
+        windowSize_ {0, 0},
+        textureSizeLeft_ {0, 0},
+        textureSizeRight_ {0, 0},
+        textureSize_ {0, 0},
+        angles_ {0, 0, 0},
+        dAngles_ {0, 0, 0},
+        distortionCaps_ {0},
+        usingDebugHmd_ {false},
+        multisampleEnabled_ {false}
+    {
+        //Oculus is a singleton and cannot be instanciated twice
+        assert(!alreadyCreated);
+
+        ovr_Initialize();
+
+        hmd_ = ovrHmd_Create(0);
+
+        if(!hmd_)
+        {
+            hmd_ = ovrHmd_CreateDebug(ovrHmd_DK1);
+            usingDebugHmd_ = true;
+
+            //Cannot create the debug hmd
+            assert(hmd_);
+
+
+             LOG("Using the debug Hmd", std::cout);
+        }
+
+        ovrHmd_GetDesc(hmd_, &hmdDesc_);
+
+        computeSizes();
+
+        distortionCaps_ = ovrDistortionCap_Chromatic | ovrDistortionCap_TimeWarp;
+
+        eyeFov_[0] = hmdDesc_.DefaultEyeFov[0];
+        eyeFov_[1] = hmdDesc_.DefaultEyeFov[1];
+
+        setOpenGLState();
+        initFBO();
+        initTexture();
+        initDepthBuffer();
+
+        computeSizes();
+        setCfg();
+        setEyeTexture();
+        ovrBool configurationRes = ovrHmd_ConfigureRendering(hmd_, &cfg_.Config, distortionCaps_, eyeFov_, eyeRenderDesc_);
+        //Cannot configure OVR rendering
+        assert(configurationRes);
+
+        ovrHmd_StartSensor(hmd_, ovrSensorCap_Orientation | ovrSensorCap_YawCorrection | ovrSensorCap_Position, ovrSensorCap_Orientation);
+
+        Oculus::alreadyCreated = true;
+    }
+    ~Oculus()
+    {
+        glDeleteFramebuffers(1, &FBOId_);
+        glDeleteTextures(1, &textureId_);
+        glDeleteRenderbuffers(1, &depthBufferId_);
+
+        ovrHmd_Destroy(hmd_);
+
+        ovr_Shutdown();
+
+        Oculus::alreadyCreated = false;
+    }
+
+    void render()
+    {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glUseProgram(0);
+
+        frameTiming_ = ovrHmd_BeginFrame(hmd_, 0);
+
+        // Bind the FBO...
+        glBindFramebuffer(GL_FRAMEBUFFER, FBOId_);
+        // Clear...
+        glClearColor(0, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        getInput();
+
+        ovrPosef eyeRenderPose[2];
+
+        for (int eyeIndex = 0; eyeIndex < ovrEye_Count; eyeIndex++)
+        {
+            ovrEyeType eye = hmdDesc_.EyeRenderOrder[eyeIndex];
+            eyeRenderPose[eye] = ovrHmd_BeginEyeRender(hmd_, eye);
+
+            glViewport(eyeTexture_[eye].OGL.Header.RenderViewport.Pos.x,
+                       eyeTexture_[eye].OGL.Header.RenderViewport.Pos.y,
+                       eyeTexture_[eye].OGL.Header.RenderViewport.Size.w,
+                       eyeTexture_[eye].OGL.Header.RenderViewport.Size.h
+                       );
+
+            // Get Projection and ModelView matrici from the device...
+            OVR::Matrix4f MV = OVR::Matrix4f::Translation(eyeRenderDesc_[eye].ViewAdjust)
+                    * OVR::Matrix4f(OVR::Quatf(eyeRenderPose[eye].Orientation).Inverted());
+
+            OVR::Matrix4f Proj = OVR::Matrix4f(ovrMatrix4f_Projection(eyeRenderDesc_[eye].Fov, 0.01f, 10000.0f, true));
+
+            glm::mat4 glmMV = Utils::ovr2glmMat(MV.Transposed());
+
+            glm::mat4 glmProj = Utils::ovr2glmMat(Proj.Transposed());
+
+            scene_->render(glmMV, glmProj);
+            Utils::GLGetError();
+
+            ovrHmd_EndEyeRender(hmd_, eye, eyeRenderPose[eye], &eyeTexture_[eye].Texture);
+        }
+
+        ovrHmd_EndFrame(hmd_);
+
+
+    }
+
+    bool isUsingDebugHmd()
+    {
+        return usingDebugHmd_;
+    }
+
+    bool isMoving() const
+    {
+        bool res = false;
+        for(int i=0; i < 3; i++)
+        {
+            res = res && Utils::isEqual(angles_[i], dAngles_[i]);
+        }
+        return !res;
+
+    }
+
+    glm::vec3 dAngles() const
+    {
+        return dAngles_;
+    }
+
+    void setDAngles(const glm::vec3 &dAngles)
+    {
+        dAngles_ = dAngles;
+    }
+
+    glm::vec3 angles() const
+    {
+        return angles_;
+    }
+    void setAngles(const glm::vec3 &angles)
+    {
+        angles_ = angles;
+    }
+    void getInput()
+    {
+        glm::vec3 oldAngles = angles_;
+
+        sensorState_ = ovrHmd_GetSensorState(hmd_, frameTiming_.ScanoutMidpointSeconds);
+
+        if(sensorState_.StatusFlags & (ovrStatus_OrientationTracked	| ovrStatus_PositionTracked))
+        {
+            ovrPosef pose = sensorState_.Predicted.Pose;
+            OVR::Quatf quat = pose.Orientation;
+
+            quat.GetEulerAngles<OVR::Axis_Y, OVR::Axis_X, OVR::Axis_Z>(&angles_.x, &angles_.y, &angles_.z);
+
+            dAngles_ = angles_ - oldAngles;
+
+            LOG("Angles: " +
+                    std::to_string(OVR::RadToDegree(angles_[0])) + ", " +
+                    std::to_string(OVR::RadToDegree(angles_[1])) + ", " +
+                    std::to_string(OVR::RadToDegree(angles_[2])) + " degrees"
+                    , std::cout);
+            LOG("Angles: " +
+                    std::to_string(angles_[0]) + ", " +
+                    std::to_string(angles_[1]) + ", " +
+                    std::to_string(angles_[2]) + " rad"
+                    , std::cout);
+
+            LOG("dAngles_: " +
+                    std::to_string(OVR::RadToDegree(dAngles_[0])) + ", " +
+                    std::to_string(OVR::RadToDegree(dAngles_[1])) + ", " +
+                    std::to_string(OVR::RadToDegree(dAngles_[2])) + " degrees"
+                    , std::cout);
+        }
+        else
+        {
+            LOG("No input data (using debug Hmd)", std::cout);
+        }
+
+    }
+
+protected:
+    void initTexture()
+    {
+        // The texture we're going to render to...
+        glGenTextures(1, &textureId_);
+        // "Bind" the newly created texture : all future texture functions will modify this texture...
+        glBindTexture(GL_TEXTURE_2D, textureId_);
+        // Give an empty image to OpenGL (the last "0")
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, textureSize_.w, textureSize_.h, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+        // Linear filtering...
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+        Utils::GLGetError();
+
+    }
+    void initFBO()
+    {
+        // We will do some offscreen rendering, setup FBO...
+        assert(textureSize_.w != 0);
+        assert(textureSize_.h != 0);
+
+        glGenFramebuffers(1, &FBOId_);
+        Utils::GLGetError();
+        //Cannot create the FBO
+        assert(FBOId_ != 0);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, FBOId_);
+        Utils::GLGetError();
+    }
+    void initDepthBuffer()
+    {
+        glGenRenderbuffers(1, &depthBufferId_);
+        Utils::GLGetError();
+        //Cannot create the depth buffer
+        assert(depthBufferId_ != 0);
+
+        glBindRenderbuffer(GL_RENDERBUFFER, depthBufferId_);
+        Utils::GLGetError();
+
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, textureSize_.w, textureSize_.h);
+        Utils::GLGetError();
+
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBufferId_);
+        Utils::GLGetError();
+
+        // Set the texture as our colour attachment #0...
+        glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, textureId_, 0);
+        Utils::GLGetError();
+
+        // Set the list of draw buffers...
+        GLenum GLDrawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
+
+        glDrawBuffers(1, GLDrawBuffers); // "1" is the size of DrawBuffers
+        Utils::GLGetError();
+
+        // Check if everything is OK...
+        GLenum check = glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER);
+
+        //There is a problem with the FBO
+        assert(check == GL_FRAMEBUFFER_COMPLETE);
+
+        // Unbind...
+        glBindRenderbuffer(GL_RENDERBUFFER, 0);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        Utils::GLGetError();
+
+    }
+    void setOpenGLState()
+    {
+        // Some state...
+
+        glDisable(GL_TEXTURE_2D);
+        glEnable(GL_DEPTH_TEST);
+        if(multisampleEnabled_)
+        {
+            glEnable(GL_MULTISAMPLE);
+        }
+
+    }
+
+    void setCfg()
+    {
+        cfg_.OGL.Header.API = ovrRenderAPI_OpenGL;
+        cfg_.OGL.Header.Multisample = multisampleEnabled_;
+        cfg_.OGL.Header.RTSize.w = windowSize_.w;
+        cfg_.OGL.Header.RTSize.h = windowSize_.h;
+
+        SDL_SysWMinfo info;
+        SDL_VERSION(&info.version);
+        SDL_bool infoRes = SDL_GetWindowWMInfo(scene_->getWindow(), &info);
+        //Cannot retrieve SDL window info
+        assert(infoRes == SDL_TRUE);
+
+#if defined(OVR_OS_WIN32)
+        cfg.OGL.Window = info.info.win.window;
+#elif defined (OVR_OS_MAC)
+        cfg.OGL.Window = info.info.cocoa.window
+#elif defined(OVR_OS_LINUX)
+        cfg_.OGL.Win = info.info.x11.window;
+        cfg_.OGL.Disp = info.info.x11.display;
+#endif
+    }
+    void setEyeTexture()
+    {
+        eyeTexture_[0].OGL.Header.API = ovrRenderAPI_OpenGL;
+        eyeTexture_[0].OGL.Header.TextureSize.w = textureSize_.w;
+        eyeTexture_[0].OGL.Header.TextureSize.h = textureSize_.h;
+        eyeTexture_[0].OGL.Header.RenderViewport.Pos.x = 0;
+        eyeTexture_[0].OGL.Header.RenderViewport.Pos.y = 0;
+        eyeTexture_[0].OGL.Header.RenderViewport.Size.h = textureSize_.h;
+        eyeTexture_[0].OGL.Header.RenderViewport.Size.w = textureSize_.w/2;
+        eyeTexture_[0].OGL.TexId = textureId_;
+
+        // Right eye the same, except for the x-position in the texture...
+        eyeTexture_[1] = eyeTexture_[0];
+        eyeTexture_[1].OGL.Header.RenderViewport.Pos.x = (textureSize_.w + 1) / 2;
+
+    }
+    void computeSizes()
+    {
+        windowSize_.w = scene_->windowWidth();  //hmdDesc_.Resolution.w;
+        windowSize_.h = scene_->windowHeight(); //hmdDesc_.Resolution.h;
+
+        LOG("FoV: " + std::to_string(Utils::radToDegree(2 * atan(hmdDesc_.DefaultEyeFov[0].UpTan))), std::cout );
+
+        textureSizeLeft_ = ovrHmd_GetFovTextureSize(hmd_, ovrEye_Left, hmdDesc_.DefaultEyeFov[0], 1.0f);
+        textureSizeRight_ = ovrHmd_GetFovTextureSize(hmd_, ovrEye_Right, hmdDesc_.DefaultEyeFov[1], 1.0f);
+        textureSize_.w = textureSizeLeft_.w + textureSizeRight_.w;
+        textureSize_.h = (textureSizeLeft_.h > textureSizeRight_.h ? textureSizeLeft_.h : textureSizeRight_.h);
+
+    }
+
+    static bool alreadyCreated;
+
+    T* scene_;
+
+    //GL
+    GLuint textureId_;
+    GLuint FBOId_;
+    GLuint depthBufferId_;
+
+    //OVR
+    ovrHmd hmd_;
+    ovrHmdDesc hmdDesc_;
+    ovrEyeRenderDesc eyeRenderDesc_[2];
+    ovrGLTexture eyeTexture_[2];
+    ovrFovPort eyeFov_[2];
+    ovrGLConfig cfg_;
+    ovrSizei windowSize_;
+    ovrSizei textureSizeLeft_;
+    ovrSizei textureSizeRight_;
+    ovrSizei textureSize_;
+    ovrFrameTiming frameTiming_;
+    ovrSensorState sensorState_;
+    glm::vec3 angles_;
+    glm::vec3 dAngles_;
+    int distortionCaps_;
+    bool usingDebugHmd_;
+    bool multisampleEnabled_;
+
+};
+
+template<class T>
+bool Oculus<T>::alreadyCreated = false;
+
+
+
+#endif
